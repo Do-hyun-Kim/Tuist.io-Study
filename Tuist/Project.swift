3b9c9686a609d86b6e60f92a838536ed0c3791e3
Do-hyun-Kim <dohyun8032@gmail.com>
2023-01-18 15:22:54 +0900

    feat: Project+Templates MakeModule Global Function, MakeSheme Global Function 추가

--- Tuist/Project.swift
+++ Tuist/Project.swift
@@ -1,5 +1,5 @@
 import ProjectDescription
-import ProjectDescriptionHelpers
+
 
 /*
                 +-------------+
@@ -22,33 +22,4 @@ import ProjectDescriptionHelpers
 // Local plugin loaded
 
 // Creates our project using a helper function defined in ProjectDescriptionHelpers
-let project = Project(
-    name: "Tuist",
-    packages: [],
-    targets: [
-        Target(
-            name: "Tuist",
-            platform: .iOS,
-            product: .app,
-            bundleId: "com.project.Tuist",
-            deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone,.ipad]),
-            infoPlist: .default,
-            sources: ["Sources/**"],
-            resources: ["Resources/**"],
-            dependencies: [],
-            settings: nil
-        ),
-        Target(
-            name: "TuistTests",
-            platform: .iOS,
-            product: .unitTests,
-            bundleId: "com.project.Tuist",
-            deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone, .ipad]),
-            sources: ["Tests/**"],
-            dependencies: [
-                .target(name: "Tuist")
-            ]
-        )
-    
-    ]
-)
+
--- Tuist/Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -1,76 +1,86 @@
 import ProjectDescription
 
-/// Project helpers are functions that simplify the way you define your project.
-/// Share code to create targets, settings, dependencies,
-/// Create your own conventions, e.g: a func that makes sure all shared targets are "static frameworks"
-/// See https://docs.tuist.io/guides/helpers/
-
-extension Project {
-    /// Helper function to create the Project for this ExampleApp
-    public static func app(name: String, platform: Platform, additionalTargets: [String]) -> Project {
-        var targets = makeAppTargets(name: name,
-                                     platform: platform,
-                                     dependencies: additionalTargets.map { TargetDependency.target(name: $0) })
-        targets += additionalTargets.flatMap({ makeFrameworkTargets(name: $0, platform: platform) })
-        return Project(name: name,
-                       organizationName: "tuist.io",
-                       targets: targets)
-    }
-
-    // MARK: - Private
-
-    /// Helper function to create a framework target and an associated unit test target
-    private static func makeFrameworkTargets(name: String, platform: Platform) -> [Target] {
-        let sources = Target(name: name,
-                platform: platform,
-                product: .framework,
-                bundleId: "io.tuist.\(name)",
-                infoPlist: .default,
-                sources: ["Targets/\(name)/Sources/**"],
-                resources: [],
-                dependencies: [])
-        let tests = Target(name: "\(name)Tests",
-                platform: platform,
-                product: .unitTests,
-                bundleId: "io.tuist.\(name)Tests",
-                infoPlist: .default,
-                sources: ["Targets/\(name)/Tests/**"],
-                resources: [],
-                dependencies: [.target(name: name)])
-        return [sources, tests]
-    }
-
-    /// Helper function to create the application target and the unit test target.
-    private static func makeAppTargets(name: String, platform: Platform, dependencies: [TargetDependency]) -> [Target] {
-        let platform: Platform = platform
-        let infoPlist: [String: InfoPlist.Value] = [
-            "CFBundleShortVersionString": "1.0",
-            "CFBundleVersion": "1",
-            "UIMainStoryboardFile": "",
-            "UILaunchStoryboardName": "LaunchScreen"
-            ]
-
-        let mainTarget = Target(
+public extension Project {
+    
+    static func makeModule(
+        name: String,
+        platform: Platform = .iOS,
+        product: Product,
+        bundleId: String? = nil,
+        packages: [Package] = [],
+        deploymentTarget: DeploymentTarget? = .iOS(targetVersion: "15.0", devices: [.iphone, .ipad]),
+        dependencies: [TargetDependency] = [],
+        sources: SourceFilesList = ["Sources/**"],
+        resources: ResourceFileElements? = nil,
+        infoPlist: InfoPlist = .default
+    ) -> Self {
+        let settings: Settings = .settings(
+            base: [:],
+            configurations: [
+                .debug(name: .debug),
+                .release(name: .release)
+            ],
+            defaultSettings: .recommended
+        )
+        
+        let appTarget: Target = Target(
             name: name,
             platform: platform,
-            product: .app,
-            bundleId: "io.tuist.\(name)",
-            infoPlist: .extendingDefault(with: infoPlist),
-            sources: ["Targets/\(name)/Sources/**"],
-            resources: ["Targets/\(name)/Resources/**"],
+            product: product,
+            bundleId: "com.project.Tuist",
+            deploymentTarget: deploymentTarget,
+            infoPlist: infoPlist,
+            sources: sources,
+            resources: resources,
             dependencies: dependencies
         )
-
-        let testTarget = Target(
+        
+        let appTestTarget: Target = Target(
             name: "\(name)Tests",
             platform: platform,
             product: .unitTests,
-            bundleId: "io.tuist.\(name)Tests",
-            infoPlist: .default,
-            sources: ["Targets/\(name)/Tests/**"],
-            dependencies: [
-                .target(name: "\(name)")
-        ])
-        return [mainTarget, testTarget]
+            bundleId: "com.project.TuistTests",
+            deploymentTarget: deploymentTarget,
+            infoPlist: infoPlist,
+            sources: ["Tests/**"],
+            dependencies: [.target(name: name)]
+        )
+        
+        let schemes: [Scheme] = [.makeScheme(target: .debug, name: name)]
+        
+        let targets: [Target] = [appTarget, appTestTarget]
+        
+        return Project(name: name,
+                       packages: packages,
+                       settings: settings,
+                       targets: targets,
+                       schemes: schemes
+        )
     }
+}
+
+
+
+public extension Scheme {
+    
+    static func makeScheme(
+        target: ConfigurationName,
+        name: String
+    ) -> Self {
+        return Scheme(
+            name: name,
+            shared: true,
+            buildAction: .buildAction(targets: ["\(name)"]),
+            testAction:.targets(
+                ["\(name)Tests"],
+                configuration: target,
+                options: .options(coverage: true, codeCoverageTargets: ["\(name)"])
+            ),
+            runAction: .runAction(configuration: target),
+            archiveAction: .archiveAction(configuration: target),
+            profileAction: .profileAction(configuration: target),
+            analyzeAction: .analyzeAction(configuration: target)
+        )
+    }
+    
 }
